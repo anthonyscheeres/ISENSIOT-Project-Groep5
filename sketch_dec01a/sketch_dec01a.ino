9b6aa65e90e0c333dc5ddbe499c3d049751e8389
Anthony <info@anthonyscheeres.nl>
2020-12-02 11:56:42 +0100

    added comments explaining the code

--- sketch_dec01a/sketch_dec01a.ino
+++ sketch_dec01a/sketch_dec01a.ino
@@ -1,24 +1,19 @@
 
-/*
- * 
- * 
- *
-*/
-
-
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
 
-//1. Change the following info
+// The static info used to connect with firebase
 #define FIREBASE_HOST "isensiot.firebaseio.com"
 #define FIREBASE_AUTH "mj33QNFCJaR92P8ywIltvjVy00jifRhytxR4J8Fe"
-#define WIFI_SSID "****"
-#define WIFI_PASSWORD "****"
 #define ID_HASH "/21232f297a57a5a743894a0e4a801fc3e033ba9cd693f9e833c72e9055d2c491"
 
+// Change this to your own wifi credentials and make sure to keep them private
+#define WIFI_SSID "*****"
+#define WIFI_PASSWORD "****"
 
 
-//2. Define FirebaseESP8266 data object for data sending and receiving
+
+// Define FirebaseESP8266 data object for data sending and receiving
 FirebaseData firebaseData;
 
 
@@ -40,11 +35,11 @@ void setup()
   Serial.println();
 
 
-  //3. Set your Firebase info
+  // Set your Firebase info
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-  //4. Enable auto reconnect the WiFi when connection lost
+  // Enable auto reconnect the WiFi when connection lost
   Firebase.reconnectWiFi(true);
 
 }
@@ -52,7 +47,7 @@ void setup()
 
 void sendJson(FirebaseJson json, String doc){
 
-//doc path esample like: doc="/test" 
+  // Doc path esample like: doc="/test" 
 
 if (Firebase.pushJSON(firebaseData, doc, json)) {
 
@@ -71,23 +66,29 @@ if (Firebase.pushJSON(firebaseData, doc, json)) {
 
 void loop()
 {
-  //initialize json object 
+  // Initialize json object 
   FirebaseJson json;
 
-  //initialize sensor data objects
+  // Initialize sensor data objects
   String time = "0";
   String ultrasoonSensor = "0";
   String lightSensor = "0";
   String moistureSensor = "0";
-  
+
+// You need to get the sensor data here 
+
+
+
+ // The data is copied to the json opject from here  
   json.set("time", time);
   json.set("ultrasoonSensor", ultrasoonSensor);
   json.set("lightSensor", lightSensor);
   json.set("moistureSensor", moistureSensor);
-
+  
+  // The is hash is used to identify devices
   sendJson(json,ID_HASH);
 
-  //60 * 60 * 1000
+  //60 * 60 * 1000 ms
   int hour = 3600000;
 
   delay(hour);
