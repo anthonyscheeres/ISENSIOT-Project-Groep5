35dba10c840ffc9c26e111d6dc43e18c195c14ef
StalenStan <stanhoenson@icloud.com>
2021-01-06 12:20:19 +0100

    added basic code for lichtsensor & vochtsensor

---
+++ kalibratie/lichtsensor/lichtsensor.ino
@@ -0,0 +1,23 @@
+/*
+ * Robotics with the BOE Shield - PhototransistorVoltage
+ * Display voltage of phototransistor circuit output connected to A3 in
+ * the serial monitor.
+ */
+
+void setup()                                 // Built-in initialization block
+{
+  Serial.begin(9600);                        // Set data rate to 9600 bps
+}
+
+void loop()                                  // Main loop auto-repeats
+{
+  Serial.print("A0 = ");                     // Display "A3 = "
+  Serial.print(volts(A0));                    // Display measured A3 volts
+  Serial.println(" volts");                  // Display " volts" & newline
+  delay(1000);                               // Delay for 1 second
+}
+                                             
+float volts(int adPin)                       // Measures volts at adPin
+{                                            // Returns floating point voltage
+ return float(analogRead(adPin)) * 5.0 / 1024.0;
+} 
