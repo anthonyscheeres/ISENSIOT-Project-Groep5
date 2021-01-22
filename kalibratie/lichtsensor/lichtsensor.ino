aedfa38d19ada1360217dda9e91be7129c5972f2
StalenStan <stanhoenson@icloud.com>
2021-01-22 12:28:14 +0100

    volt to lux formula added

--- kalibratie/lichtsensor/lichtsensor.ino
+++ kalibratie/lichtsensor/lichtsensor.ino
@@ -10,17 +10,20 @@ void loop()
 
 void measurement(int times)
 {
-  float total = 0.00;
+  double total = 0.00;
   for (int i = 0; i < times; i++)
   {
-    float volt = float(analogRead(A0)) * 5.0 / 1024.0;
+    double volt = float(analogRead(A0)) * 5.0 / 1024.0;
+    double lux = (-1750.8 * (volt * volt)) + (15154 * volt) - 1178.7;
     total = total + volt;
     Serial.print("A0 = ");
-    Serial.print(volt);
-    Serial.println(" volts");
+    Serial.print(lux);
+    Serial.println(" lux");
+//    Serial.print(volt);
+//    Serial.println(" volts");
     delay(1000);
   }
-  float average = total / times;
+  double average = total / times;
   Serial.print("Average = ");
   Serial.println(average);
 }
